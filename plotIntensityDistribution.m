5ddd2c66416975948856faa44845c2dc1e4c200b
Serena Ding <sding1@ld-rendres11.bc.ic.ac.uk>
2017-01-19 18:54:33 +0000

    add multi panel speed heatmap comparison

--- plotIntensityDistribution.m
+++ plotIntensityDistribution.m
@@ -2,8 +2,8 @@
 % larve data
 
 % set directory to the relevant skeleton file
-directory = '/data2/shared/data/Results/recording 38.8 green 100-200 TIFF/recording 38.8 green_X1_skeletons.hdf5';
+directory = '/data2/shared/data/Results/recording 44.2 green 100-250 TIFF/recording 44.2 green_X1_skeletons.hdf5';
 % load intensities data
 BlobFeats = h5read(directory,'/blob_features');
 % plot distribution
-histogram(BlobFeats.threshold)
+histogram(BlobFeats.intensity_mean)
